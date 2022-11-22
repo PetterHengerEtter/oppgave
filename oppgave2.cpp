de589d6f4067e6fbc81d9cd4d533a6be1576ba72
Petter <petterjonli@outlook.com>
2022-11-22 10:32:11 +0100

    oppgave2 update

--- oppgave2.cpp
+++ oppgave2.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 int main() {
-  cout << "jeg er best!";
+    // comments blir brukt for en mer oversiktelig kode
+  cout << "jeg er best!\n\n";
+  cout << "idag lærer jeg meg c++\t";
+  cout <<"skriver masse shit\n\n"
+
+ // varliabler 
+ int myNum = 15;
+
+ cout << myNum;
+ int myNum = 15;  // myNum is 15
+    myNum = 10;  // Now myNum is 10
+    cout << myNum;  // Outputs 10
+    int myNum = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String (text)
+    bool myBoolean = true;       // Boolean (true or false)
+    
+    int myAge = 17;
+    cout <<"I am" <<myAge <<"years old";
   return 0;
 }
